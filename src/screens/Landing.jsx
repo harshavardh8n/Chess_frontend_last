fd1edf0de178879c58547b805c7a64e87d4f88d8
Harshavardhan Gaikwad <115050775+harshavardh8n@users.noreply.github.com>
2024-12-19 09:59:09 +0530

    Update Landing.jsx

--- src/screens/Landing.jsx
+++ src/screens/Landing.jsx
@@ -6,16 +6,16 @@ const Landing = () => {
 
   return (
     <>
-      <div className="bg-[#212121] h-screen w-full">
-        <div className="mt-2 flex gap-4">
-          <div className="w-1/3 mt-20 ml-20">
-            <img src={chess} alt="Chess Illustration" />
+      <div className="bg-[#212121] lg:h-screen h-fit w-full">
+        <div className="mt-2 flex gap-4 lg:flex-row flex-col">
+          <div className="lg:w-1/3 mt-20 lg:ml-20">
+            <img src={chess} alt="Chess" />
           </div>
-          <div className="m-10 h-[60vh] w-1/2 flex flex-col justify-center">
+          <div className="m-10 lg:h-[60vh] lg:w-1/2 flex flex-col justify-center">
             <h1 className="text-3xl font-bold text-white mx-auto">
               Play chess online on the #3 site
             </h1>
-            <div className="mt-5 bg-green-600 w-1/4 p-2 rounded-md font-bold mx-auto text-center">
+            <div className="mt-5 bg-green-600 lg:w-1/4 p-2 rounded-md font-bold mx-auto text-center">
               <button
                 onClick={() => {
                   navigate("/game");
